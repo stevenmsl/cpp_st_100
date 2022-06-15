67a8c6b15bd4f65f6f139f86a0c3d3d1c4918b24
Steven Lin <steven_lx@outlook.com>
2022-06-15 11:12:22 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -1,20 +1,13 @@
 #include "solution.h"
 #include "util.h"
-#include <unordered_set>
-#include <numeric>
-#include <iterator>
-#include <unordered_map>
-#include <deque>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <stack>
+
 using namespace sol104;
 using namespace std;
 
 /*takeaways
   - use recursion
   - pay attention to edge cases
+  - time complexity O(n)
 */
 bool Solution::isSameTree(Node *p, Node *q)
 {
